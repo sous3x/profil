3c29670ce4d5599fecac9744521cb02f669324b5
sous3x <bochjocelin@gmail.com>
2025-10-23 23:30:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function onPaypal(){
+    if(window.confirm("Voulez vous me faire un don ?")){
+        window.open("https://paypal.me/Sous3x")
+    }
+}
+
+function onDiscord(){
+    if(window.confirm("Voulez vous rejoindre la communauté ?")){
+        window.open("https://discord.gg/eraVJJQR")
+    }
+}
